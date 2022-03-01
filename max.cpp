9bcbf731cb5a6ac85b05825b3d611789bff5045f
fanjiahui <1203394464@qq.com>
2022-03-01 21:49:18 +0800

    first commit

---
+++ max.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+float max(float a,float b)
+{
+    if(a>b)
+    {
+        return a;
+    }
+    else
+    {
+        return b;
+    }
+}
+
+int main()
+{
+    cout<<max(3,8)<<endl;
+    return 0;
+}
